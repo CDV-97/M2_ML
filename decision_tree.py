e4341b84eca400db51a7cb228a74805b27a60d88
CDV-97 <a01747037@tec.mx>
2026-08-31 18:22:58 -0600

    Add DecisionTree class with initialization parameters

--- decision_tree.py
+++ decision_tree.py
@@ -8,3 +8,9 @@ class Node:
 
     def is_leaf_node(self):
         return self.value is not None
+
+class DecisionTree:
+    def __init__(self, max_depth=5, min_samples_split=2):
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.root = None
